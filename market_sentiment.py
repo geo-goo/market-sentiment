78c1bbae7135e6a2f3fea0504e5fbe5fee78ce1d
geo <34396454+geo-goo@users.noreply.github.com>
2023-06-26 19:40:41 +0800

    upload

--- market_sentiment.py
+++ market_sentiment.py
@@ -10,21 +10,9 @@ import matplotlib.pyplot as pt
 nltk.data.path.append("/home/geo/Downloads/Trade/text-mining-bot/ntlk_data/")
 # nltk.download('vader_lexicon',download_dir="./ntlk_data/")
 
-# Text data and corresponding sentiment labels
-texts = ["This is a great product! I really like it.",
-         "I'm feeling very frustrated as this investment resulted in a loss.",
-         "The market outlook is positive and the stock prices are rising.",
-         "The company released a major positive news.",
-         "Investor sentiment is low, and they have a pessimistic view of the market.",
-         "It's not really prefect."]
-
-# Sentiment analysis and sentiment prediction
-sia = SentimentIntensityAnalyzer()
-sentiments = []
-
 class emotion_index :
-    def __init__(self,emotion) -> None:
-        self.emotion = emotion
+    def __init__(self,texts) -> None:
+        self.texts = texts 
 
     def emotion_score(self):
         # emotion_weights = {'negative': 0.4, 'neutral': 0.2,'positive': 0.6}
@@ -39,22 +27,34 @@ class emotion_index :
     def emotion_ratios(self):
         emotion = np.array(self.emotion)
         result = emotion / emotion.sum()
-        return result
+        return np.round(result,2)
+    
+    def main(self):
+        # Sentiment analysis and sentiment prediction
+        sia = SentimentIntensityAnalyzer()
+        for text in self.texts:
+            self.emotion = []
+            sentiment = sia.polarity_scores(text)
+            for key , value in sentiment.items():
+                if key in 'compound' :
+                    compound = value
+                    continue
+                self.emotion.append(value)
+            # print(sentiment['neg'] , sentiment['neu'] , sentiment['pos'])
+            _ , __ , ___ = self.emotion_score() , self.emotion_ratios() , self.emotion_variation()
+            print(f'emotion score : {_:.2f} , emotion ratio : {__} , emotion variation : {___:.2f} , emotion compound :{compound:.2f}')
 
 if __name__ == '__main__':
 
-    for text in texts:
-        emotion = []
-        sentiment = sia.polarity_scores(text)
-        for key , value in sentiment.items():
-            if key in 'compound' :
-                compound = value
-                continue
-            emotion.append(value)
-        # print(sentiment['neg'] , sentiment['neu'] , sentiment['pos'])
-        e = emotion_index(emotion)
-        _ , __ , ___ = e.emotion_score() , e.emotion_ratios() , e.emotion_variation()
-        print(f'{_:.2f} , {__} , {___:.2f} , {compound:.2f}')
+    # Text data and corresponding sentiment labels
+    texts = ["This is a great product! I really like it.",
+            "I'm feeling very frustrated as this investment resulted in a loss.",
+            "The market outlook is positive and the stock prices are rising.",
+            "The company released a major positive news.",
+            "Investor sentiment is low, and they have a pessimistic view of the market.",
+            "It's not really prefect."]
+
+    emotion_index(texts).main()
     
 
 # Visualize sentiment scores and predictions
